4e9cc80ca93446046ef080c93cb810a2089d3422
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-07-13 10:49:20 -0400

    Making the frontend

--- src/app.js
+++ src/app.js
@@ -1,36 +1,29 @@
 const express = require('express')
+const path = require('path')
 
-console.log(__dirname)
-console.log(__filename)
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello Express!!')
-})
+publicDirectory = path.join(__dirname, '../public')
+app.use(express.static(publicDirectory))
 
-app.get('/help', (req, res) => {
-    res.send('Help page')
-})
 
-app.get('/about', (req, res) => {
-    res.send('<h1>Find more details here</h1>')
-})
+
+// app.get('/help', (req, res) => {
+//     res.send('help')
+// })
+
+// app.get('/about', (req, res) => {
+// res.send('about')
+// })
 
 app.get('/weather', (req, res) => {
     res.send({
-        forecast: {
-            latitude: 54.876,
-            longitude: -23.34
-        },
-        location: "Ney York"
+        lattitude: 123,
+        longitude: 456
     })
 })
 
-
 app.listen(3000, () => {
-    console.log('server is running on port 3000')
+    console.log('server running on port 3000')
 })
-//app.com
-//app.com/help
-//app.com/about
