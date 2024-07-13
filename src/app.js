6630a51932f924927d8b9dde4839ee7ea5b7f3f3
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-07-13 18:05:36 -0400

    handlebars used to pass the values dynamically

--- src/app.js
+++ src/app.js
@@ -1,21 +1,31 @@
 const express = require('express')
 const path = require('path')
 
-
 const app = express();
-
 publicDirectory = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectory))
 
+app.get('/', (req, res) => {
+    res.render('index',{
+        title: 'Weather App',
+        author: "abc"
+    })
+})
 
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title:'Help Page'
+    })
+})
 
-// app.get('/help', (req, res) => {
-//     res.send('help')
-// })
-
-// app.get('/about', (req, res) => {
-// res.send('about')
-// })
+app.get('/about', (req, res) => {
+res.render('about', {
+    title: 'About Page',
+    details: 'Find more details about me'
+})
+})
 
 app.get('/weather', (req, res) => {
     res.send({
