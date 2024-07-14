be49f02f4cde480a7e0c97b66b98a12611db7832
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-07-13 22:46:18 -0400

    more about hbs partials

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 
 const app = express();
-publicDirectory = path.join(__dirname, '../public')
 
+//Defining the paths for express config
+publicDirectory = path.join(__dirname, '../public')
+viewPath = path.join(__dirname,'../templates/views')
+partialsPath = path.join(__dirname, '../templates/partials')
+
+//setup handlebars engine and view location
 app.set('view engine', 'hbs');
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
 app.use(express.static(publicDirectory))
 
-app.get('/', (req, res) => {
+app.get('', (req, res) => {
     res.render('index',{
         title: 'Weather App',
         author: "abc"
